772db70943448119edf566f3a5a5479bee986f14
Karem Andrade <karemsilva.andrade@gmail.com>
2024-03-05 20:06:45 -0300

    Commit inicial

---
+++ src/test/java/AprovacaoAlunoTest.java
@@ -0,0 +1,11 @@
+package test.java;
+
+public class AprovacaoAlunoTest {
+
+    class AprovacaoAluno{
+
+        private String verificarAprovacao(int frequencia, int notaFinal){
+            return "";
+        }
+    }
+}
