7e85957e286b3f19ec32a4692b7142878697d5ca
Karem Andrade <karemsilva.andrade@gmail.com>
2024-03-06 20:53:50 -0300

    criado cenarios de teste completo

--- src/main/java/puc/minas/Aluno.java
+++ src/main/java/puc/minas/Aluno.java
@@ -1,16 +1,29 @@
 package main.java.puc.minas;
 
 public class Aluno {
-    private int frequencia;
-    private int notaFinal;
-    private int notaEspecial;
+    private double frequencia;
+    private double notaFinal;
+    private double notaEspecial;
 
-    public Aluno(int frequencia, int notaFinal, int notaEspecial) {
+    public Aluno(double frequencia, double notaFinal, double notaEspecial) {
         this.frequencia = frequencia;
         this.notaFinal = notaFinal;
         this.notaEspecial = notaEspecial;
     }
     public String determinarAprovacao() {
-        return "";
+        if(frequencia < 75)
+            return "Reprovado";
+        else if (frequencia < 0 || notaFinal < 0
+                || notaEspecial < 0)
+            return "Reprovado";
+        else if (frequencia > 75 && notaFinal < 60 &&
+                notaEspecial >= 0 && notaEspecial <= 100)
+            return "Reprovado";
+        else if (notaFinal >= 60)
+            return "Aprovado";
+        else if ((notaFinal + notaEspecial) / 2 >= 60)
+            return "Aprovado";
+        else
+            return "Reprovado";
     }
 }
--- src/test/java/AlunoTest.java
+++ src/test/java/AlunoTest.java
@@ -1,25 +1,43 @@
 package test.java;
 
 import main.java.puc.minas.Aluno;
-import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class AlunoTest {
-
-
-    Aluno aluno;
-
-    @Before
-    public void setUp(){
-        aluno = new Aluno(60,70,80);
+    @Test
+    public void testAlunoReprovadoPelaFrequencia() {
+        Aluno aluno1 = new Aluno(60, 70, 80);
+        assertEquals("Reprovado", aluno1.determinarAprovacao());
+    }
+    @Test
+    public void testAlunoAprovadoPelaNotaFinal() {
+        Aluno aluno2 = new Aluno(80, 65, 80);
+        assertEquals("Aprovado", aluno2.determinarAprovacao());
     }
 
     @Test
-    public void testAlunoReprovadoPelaFrequencia() {
-        assertEquals("Reprovado", aluno.determinarAprovacao());
+    public void testAlunoReprovadoPelosDemaisCasos() {
+        Aluno aluno3 = new Aluno(80, 41, 70);
+        assertEquals("Reprovado", aluno3.determinarAprovacao());
     }
 
+    @Test
+    public void testAlunoReprovadoPelaFrequenciaNegativa() {
+        Aluno aluno4 = new Aluno(-1, 70, 80);
+        assertEquals("Reprovado", aluno4.determinarAprovacao());
+    }
 
+    @Test
+    public void testAlunoReprovadoPelaNotaFinalNegativa() {
+        Aluno aluno5 = new Aluno(80, -30, 70);
+        assertEquals("Reprovado", aluno5.determinarAprovacao());
+    }
+
+    @Test
+    public void testAlunoReprovadoPelaNotaEspecialNegativa() {
+        Aluno aluno6 = new Aluno(80, 70, -40);
+        assertEquals("Reprovado", aluno6.determinarAprovacao());
+    }
 }
